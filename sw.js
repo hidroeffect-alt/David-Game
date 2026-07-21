523bf056c4ae470bfe05e37e0bf62878b80d3e95
hidroeffect-alt <hidroeffect@gmail.com>
2026-07-21 18:18:55 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-// disabled in v7 to avoid stale cache issues
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
